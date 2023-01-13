78fc4325bfa2903f2fed82249d856d579f1277a5
mihail.i <chumbam@mail.ru>
2023-01-13 14:52:20 +0100

    Setting up the Android Theme

---
+++ shared/src/commonMain/kotlin/com/isaev/mytranslator_kmm/core/presentation/Colors.kt
@@ -0,0 +1,9 @@
+package com.isaev.mytranslator_kmm.core.presentation
+
+object Colors {
+    const val LightBlue = 0xFFA8A5BB
+    const val LightGrayBlue = 0xFFF6F4F4
+    const val AccentViolet = 0xFF5643C9
+    const val TextBlack = 0xFF111111
+    const val DarkGray = 0xFF282C31
+}
