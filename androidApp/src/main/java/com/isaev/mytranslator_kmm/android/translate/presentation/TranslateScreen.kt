b247b45ba0c0f1d50a15cdfeed22efd5746eddf3
user <user@192.168.1.79>
2023-01-21 12:26:50 +0100

    Added Translation Text Field and test them

--- androidApp/src/main/java/com/isaev/mytranslator_kmm/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/isaev/mytranslator_kmm/android/translate/presentation/TranslateScreen.kt
@@ -1,14 +1,23 @@
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.isaev.mytranslator_kmm.android.translate.presentation
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.isaev.mytranslator_kmm.android.translate.presentation.components.LanguageDropDown
 import com.isaev.mytranslator_kmm.android.translate.presentation.components.SwapLanguageButton
+import com.isaev.mytranslator_kmm.android.translate.presentation.components.TranslateTextField
 import com.isaev.mytranslator_kmm.translate.presentation.TranslateEvent
 import com.isaev.mytranslator_kmm.translate.presentation.TranslateState
 
@@ -17,6 +26,7 @@ fun TranslateScreen(
     state: TranslateState,
     onEvent: (TranslateEvent) -> Unit
 ) {
+    val context = LocalContext.current
     Scaffold(
         floatingActionButton = {
 
@@ -68,6 +78,46 @@ fun TranslateScreen(
                         }
                     )
                 }
+            }
+            item {
+                val clipboardManager = LocalClipboardManager.current
+                val keyboardController = LocalSoftwareKeyboardController.current
+                TranslateTextField(
+                    fromText = state.fromText,
+                    toText = state.toText,
+                    isTranslating = state.isTranslating,
+                    fromLanguage = state.fromLanguage,
+                    toLanguage = state.toLanguage,
+                    onTranslateClick = {
+                        keyboardController?.hide()
+                        onEvent(TranslateEvent.Translate)
+                    },
+                    onTextChange = {
+                        onEvent(TranslateEvent.ChangeTranslationText(it))
+                    },
+                    onCopyClick = { text ->
+                        clipboardManager.setText(
+                            buildAnnotatedString {
+                                append(text = text)
+                            }
+                        )
+                        Toast.makeText(
+                            context,
+                            context.getString(com.isaev.mytranslator_kmm.android.R.string.copied_to_clipboard),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    },
+                    onCloseClick = {
+                        onEvent(TranslateEvent.CloseTranslation)
+                    },
+                    onSpeakerClick = {
+
+                    },
+                    onTextFieldClick = {
+                        onEvent(TranslateEvent.EditTranslation)
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                )
             }
         }
 
---
+++ androidApp/src/main/java/com/isaev/mytranslator_kmm/android/translate/presentation/components/SmallLanguageIcon.kt
@@ -0,0 +1,21 @@
+package com.isaev.mytranslator_kmm.android.translate.presentation.components
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.isaev.mytranslator_kmm.core.presentation.UiLanguage
+
+@Composable
+fun SmallLanguageIcon(
+    language: UiLanguage,
+    modifier: Modifier = Modifier
+) {
+    AsyncImage(
+        model = language.drawableRes,
+        contentDescription = language.language.langName,
+        modifier = modifier.size(25.dp)
+    )
+
+}
